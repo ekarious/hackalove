e9b1fcb3258d22c4b84aff4dce50869e7b327761
Marlene Maisonniaux <design.msx@gmail.com>
2018-05-17 16:37:24 +0200

    boutons valider et annuler

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ListePersonnes from "./components/ListePersonnes";
+import BoutonValider from "./components/BoutonValider";
+import BoutonAnnuler from "./components/BoutonAnnuler";
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <BoutonValider /> <BoutonAnnuler />
         <ListePersonnes />
       </div>
     );
