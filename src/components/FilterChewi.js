4270701eb252abf5db316184dfdf3bf2ec1ed0ac
Romain Denamur <romain.denamur@gmail.com>
2018-05-17 23:17:39 +0200

    tri dynamique des especes

--- src/components/FilterChewi.js
+++ src/components/FilterChewi.js
@@ -1,18 +1,105 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import logo from "../images/logo_chewie.png";
+import axios from "axios";
 
-
+const image = {
+  width: "100%"
+};
 
 class FilterChewi extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-              
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      Personnes: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get(`https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json`)
+      .then(res => {
+        const Personnes = res.data;
+        this.setState({ Personnes });
+      });
+  }
+
+  render() {
+    const speciseList = this.state.Personnes.map(person => person.species);
+    const personnes = Array.from(new Set(speciseList));
+    const characters = personnes.map(el => <option>{el}</option>);
+
+    return (
+      <div>
+        <ul />
+        <img src={logo} style={image} />
+        <Form>
+          <FormGroup>
+            <Label for="exampleSelect">
+              Choisier l'espce de votre futur moitier
+            </Label>
+            <Input type="select" name="select" id="exampleSelect">
+              {characters}
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelectMulti">Select Multiple</Label>
+            <Input
+              type="select"
+              name="selectMulti"
+              id="exampleSelectMulti"
+              multiple
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleFile">File</Label>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above
+              input. It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </FormGroup>
+          <FormGroup tag="fieldset">
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option one is this and
+                that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option two can be something
+                else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type="radio" name="radio1" disabled /> Option three is
+                disabled
+              </Label>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" /> Check me out
+            </Label>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default FilterChewi;
