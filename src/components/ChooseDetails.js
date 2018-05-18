eb8d7fb6661e54c8c1058d3d84a2352c0e21e3ce
Marlene Maisonniaux <design.msx@gmail.com>
2018-05-18 09:47:27 +0200

    insertion card bootstrap avec nom ID

--- src/components/ChooseDetails.js
+++ src/components/ChooseDetails.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Table, Container, Row, Col } from "reactstrap";
 import axios from "axios";
-import "../ChooseDetails.css";
+import "../components/ChooseDetails.css";
 import {
   Card,
   Button,
@@ -16,30 +15,42 @@ import {
 class ChooseDetails extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      Personne: { id: 25 }
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        `https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/id/${
+          this.state.Personne.id
+        }.json`
+      )
+      .then(res => {
+        const Personne = res.data;
+        console.log(res.data);
+        this.setState({ Personne });
+      });
   }
   render() {
     return (
-      <CardGroup>
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-      </CardGroup>
+      <div className="suggestioncard">
+        <h5>Veux-tu être mon stormtrompeur?</h5>
+        <CardGroup>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src={this.state.Personne.image}
+              alt={this.state.Personne.name}
+            />
+            <CardBody>
+              <CardTitle>{this.state.Personne.name}</CardTitle>
+              <CardSubtitle>{this.state.Personne.gender}</CardSubtitle>
+            </CardBody>
+          </Card>
+        </CardGroup>
+      </div>
     );
   }
 }
